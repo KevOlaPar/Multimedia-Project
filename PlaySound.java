1b9f79fc8d5ac8e96a6caf4cd5ca8e9009512b61
Abiola Johnson <biola.johnson@yahoo.com>
2022-04-17 18:46:16 -0700

    Add files via upload
    audio functionality

---
+++ PlaySound.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class PlaySound {
+  public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+    try
+    {
+        Sound audioPlayer = 
+                        new Sound(args[0]);
+          
+        audioPlayer.play();
+        Scanner scanner = new Scanner(System.in);
+          
+        while (true)
+        {
+            System.out.println("1. pause");
+            System.out.println("2. resume");
+            System.out.println("3. restart");
+            System.out.println("4. stop");
+            System.out.println("5. Jump to specific time");
+            int choice = scanner.nextInt();
+            audioPlayer.goToChoice(choice);
+            if (choice == 4)
+            break;
+        }
+        scanner.close();
+    } 
+      
+    catch (Exception ex) 
+    {
+        System.out.println("Error with playing sound.");
+        ex.printStackTrace();
+      
+      }
+
+  }
+}
