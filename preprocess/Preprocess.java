63ac63eee6593576938412578cdead380e455547
Kaiyuan Yu <kaiyuany@usc.edu>
2022-05-03 02:55:57 -0700

    finished everything except for executing python from java code

--- preprocess/Preprocess.java
+++ preprocess/Preprocess.java
@@ -1,18 +1,25 @@
 package preprocess;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 import audio.AudioFrame;
 import audio.AudioFrameReader;
+import com.profesorfalken.jpowershell.PowerShell;
+import com.profesorfalken.jpowershell.PowerShellNotAvailableException;
+import com.profesorfalken.jpowershell.PowerShellResponse;
 import media_player2.Audio;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import video.Frame;
 import video.Scene;
 import video.Shot;
 import video.VideoFrameReader;
+import org.json.simple.JSONObject;
+
 
 /*
  * Preprocess the video.
@@ -20,11 +27,20 @@ import video.VideoFrameReader;
  * Divide frames into shots and scenes. 
  */
 public class Preprocess {
-	public static enum Advertisement{
-		NONE, STARBUCKS, SUBWAY, MCDONALDS, NFL, AMERICAN_EAGLE, HARD_ROCK_CAFE
-	}
 	private String inputRgbFile, inputWavFile, outputRgbFile, outputWavFile;
-	private Advertisement detectedAd;
+	public static Map<String, String> adRgbFile;
+	public static Map<String, String> adWavFile;
+	private Map<Integer, String> logoFrame; //the frame in which logo appeared
+	private JSONObject boundingBoxes;
+	String[] logoNames = {
+			"Starbucks",
+			"Subway",
+			"Mcdonalds",
+			"NFL",
+			"AmericanEagle",
+			"HardRockCafe"
+	};
+
 	
 	public Preprocess(String inputRgb, String inputWav, String outputRgb, String outputWav) {
 		inputRgbFile = inputRgb;
@@ -32,7 +48,23 @@ public class Preprocess {
 		outputRgbFile = outputRgb;
 		outputWavFile = outputWav;
 
-		detectedAd = Advertisement.NONE;
+		adRgbFile = new HashMap<>();
+		adRgbFile.put("Starbucks", "C:\\Users\\Kevin Yu\\Downloads\\dataset-001\\dataset\\Ads\\Starbucks_Ad_15s.rgb");
+		adRgbFile.put("Subway", "C:\\Users\\Kevin Yu\\Downloads\\dataset-001\\dataset\\Ads\\Subway_Ad_15s.rgb");
+		adRgbFile.put("Mcdonalds", "C:\\Users\\Kevin Yu\\Downloads\\dataset-002\\dataset2\\Ads\\mcd_Ad_15s.rgb");
+		adRgbFile.put("NFL", "C:\\Users\\Kevin Yu\\Downloads\\dataset-002\\dataset2\\Ads\\nfl_Ad_15s.rgb");
+		adRgbFile.put("AmericanEagle", "C:\\Users\\Kevin Yu\\Downloads\\dataset-003\\dataset3\\Ads\\ae_ad_15s.rgb");
+		adRgbFile.put("HardRockCafe", "C:\\Users\\Kevin Yu\\Downloads\\dataset-003\\dataset3\\Ads\\hrc_ad_15s.rgb");
+
+		adWavFile = new HashMap<>();
+		adWavFile.put("Starbucks", "C:\\Users\\Kevin Yu\\Downloads\\dataset-001\\dataset\\Ads\\Starbucks_Ad_15s.wav");
+		adWavFile.put("Subway", "C:\\Users\\Kevin Yu\\Downloads\\dataset-001\\dataset\\Ads\\Subway_Ad_15s.wav");
+		adWavFile.put("Mcdonalds", "C:\\Users\\Kevin Yu\\Downloads\\dataset-002\\dataset2\\Ads\\mcd_Ad_15s.wav");
+		adWavFile.put("NFL", "C:\\Users\\Kevin Yu\\Downloads\\dataset-002\\dataset2\\Ads\\nfl_Ad_15s.wav");
+		adWavFile.put("AmericanEagle", "C:\\Users\\Kevin Yu\\Downloads\\dataset-003\\dataset3\\Ads\\ae_ad_15s.wav");
+		adWavFile.put("HardRockCafe", "C:\\Users\\Kevin Yu\\Downloads\\dataset-003\\dataset3\\Ads\\hrc_ad_15s.wav");
+
+		logoFrame = new HashMap<>();
 	}
 	
 	/*
@@ -187,10 +219,8 @@ public class Preprocess {
 	        	}
 	        }
 		} catch (UnsupportedAudioFileException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -208,15 +238,35 @@ public class Preprocess {
 		byte[] frameBytes = new byte[Frame.FRAME_WIDTH * Frame.FRAME_HEIGHT * 3];
 		int sceneIndex = 0;
 		Scene curScene = scenes.get(sceneIndex++);
+
+		String logoSeen = null;//track which logo we have seen in the frames, null = haven't seen an ad
+
 		while((frame = frameReader.nextFrame()) != null){
 			int frameIndex = frame.getFrameIndex();
+			if(logoFrame.get(frameIndex) != null){
+				logoSeen = logoFrame.get(frameIndex);
+			}
 			//if the current frame is the start of an ad scene,
 			//write the detected logo ad instead,
 			//and set frame index to the end of the ad scene.
 			if(frameIndex == curScene.getStartIndex() && curScene.isAd()){
 				frameReader.setFrameIndex(curScene.getEndIndex());
-				//TODO: write ad file bytes into the output file
 
+				//if we have seen a logo in previous frames, write the ad for that logo
+				if(logoSeen != null){
+					VideoFrameReader adFrameReader = new VideoFrameReader(adRgbFile.get(logoSeen));
+					Frame adFrame;
+					while((adFrame = adFrameReader.nextFrame()) != null){
+						double[][] red = adFrame.getRedChannel();
+						writeChannelToFile(videoOutput, red);
+						double[][] green = adFrame.getGreenChannel();
+						writeChannelToFile(videoOutput, green);
+						double[][] blue = adFrame.getBlueChannel();
+						writeChannelToFile(videoOutput, blue);
+					}
+					adFrameReader.closeFile();
+					logoSeen = null;//reset the logo seen
+				}
 				//advancing scene index
 				curScene = scenes.get(sceneIndex++);
 				continue;
@@ -226,11 +276,32 @@ public class Preprocess {
 				curScene = scenes.get(sceneIndex++);
 			}
 			//write each color channel separately to the new file
-			//TODO: change some pixels to show the bounding box of the logo
+			int[][] boundingBox = getBoundingBox(String.valueOf(frameIndex));
+
 			double[][] red = frame.getRedChannel();
 			writeChannelToFile(videoOutput, red);
+
 			double[][] green = frame.getGreenChannel();
+			if(boundingBox != null){
+				int tl_x = boundingBox[0][0];
+				int tl_y = boundingBox[0][1];
+				int br_x = boundingBox[1][0];
+				int br_y = boundingBox[1][1];
+				for(int i=tl_x; i<br_x; i++){
+					green[i][tl_y] = 255;
+				}
+				for(int i=tl_x; i<br_x; i++){
+					green[i][br_y] = 255;
+				}
+				for(int i=tl_y; i<br_y; i++){
+					green[tl_x][i] = 255;
+				}
+				for(int i=tl_y; i<br_y; i++){
+					green[br_x][i] = 255;
+				}
+			}
 			writeChannelToFile(videoOutput, green);
+
 			double[][] blue = frame.getBlueChannel();
 			writeChannelToFile(videoOutput, blue);
 		}
@@ -257,7 +328,7 @@ public class Preprocess {
 	/*
 	 * write output wav file with replaced ads
 	 */
-	public void writeWavFile(List<Scene> scenes) throws IOException {
+	public void writeWavFile(List<Scene> scenes) throws IOException, UnsupportedAudioFileException {
 		RandomAccessFile audioOutput = new RandomAccessFile(outputWavFile, "rw");
 		RandomAccessFile audioInput = new RandomAccessFile(inputWavFile, "r");
 
@@ -270,14 +341,31 @@ public class Preprocess {
 		int sceneIndex = 0, frameIndex = 0;
 		Scene curScene = scenes.get(sceneIndex++);
 		int bytesRead = 0;
+
+		String logoSeen = null;//track which logo we have seen in the frames, null = haven't seen an ad
+
 		while((bytesRead = audioInput.read(audioBuffer)) > 0){
+			if(logoFrame.get(frameIndex) != null){
+				logoSeen = logoFrame.get(frameIndex);
+			}
 			//if the current frame is the start of an ad scene,
 			//write the detected ad file instead,
 			//and set frame index to the end of the ad scene.
 			if(frameIndex == curScene.getStartIndex() && curScene.isAd()){
 				frameIndex = curScene.getEndIndex();
 				audioInput.skipBytes((curScene.getEndIndex() - curScene.getStartIndex()) * AudioFrame.BYTES_PER_FRAME);
-				//TODO: write ad file bytes into the output file
+
+				// if a logo was seen in previous frames, write the wav file of that ad
+				if(logoSeen != null){
+					AudioFrame adFrame;
+					AudioFrameReader adReader = new AudioFrameReader(adWavFile.get(logoSeen));
+					while((adFrame = adReader.nextFrame()) != null){
+						byte[] adBuffer = adFrame.getFrameBytes();
+						audioOutput.write(adBuffer);
+					}
+					adReader.closeFile();
+					logoSeen = null;
+				}
 
 				//advancing scene index
 				curScene = scenes.get(sceneIndex++);
@@ -312,6 +400,53 @@ public class Preprocess {
 		return ret;
 	}
 
+	public void parseJsonFile() throws IOException, ParseException {
+
+		Object obj = new JSONParser().parse(new FileReader("detection/d1_final.txt"));
+		JSONObject adDetectionResult = (JSONObject)obj;
+		JSONArray logos = (JSONArray)adDetectionResult.get("logos");
+		Iterator iter = logos.iterator();
+		while(iter.hasNext()){
+			JSONObject logo = (JSONObject)iter.next();
+			for(String logoName : logoNames){
+				if(logo.get(logoName) != null){
+					int frameIndex = ((Long)logo.get(logoName)).intValue();
+					logoFrame.put(frameIndex, logoName);
+				}
+			}
+		}
+
+		this.boundingBoxes = (JSONObject) adDetectionResult.get("frames");
+	}
+
+	/*
+	 * get bounding box top right and bottom right indices given frame index
+	 */
+	public int[][] getBoundingBox(String frameIndex){
+
+		if(boundingBoxes.get(frameIndex) == null){
+			return null;
+		}
+		JSONArray arr = (JSONArray)boundingBoxes.get(frameIndex);
+
+		Iterator iter1 = arr.iterator();
+		JSONArray topLeft = (JSONArray)iter1.next();
+		JSONArray botRight = (JSONArray)iter1.next();
+
+		Iterator iter2 = topLeft.iterator();
+		int tl_x = ((Long)iter2.next()).intValue();
+		int tl_y = ((Long)iter2.next()).intValue();
+
+		Iterator iter3 = botRight.iterator();
+		int br_x = ((Long)iter3.next()).intValue();
+		int br_y = ((Long)iter3.next()).intValue();
+		br_x = br_x > 479 ? 479 : br_x;
+		br_y = br_y > 269 ? 269 : br_y;
+		return new int[][]{
+				{tl_x, tl_y},
+				{br_x, br_y}
+		};
+	}
 	/*
 	 *  args[0] = input rgb file
 	 *  args[1] = input wav file
@@ -321,9 +456,15 @@ public class Preprocess {
 	 *  args[5] = upper audio level threshold
 	 *  args[6] = lower audio level threshold
 	 */
-	public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
+	public static void main(String[] args) throws IOException, UnsupportedAudioFileException, ParseException {
 		long timeStart = System.currentTimeMillis();
+		//TODO: run powershell command to run logo detection logo python script
+//		String command = "powershell.exe $env:GOOGLE_APPLICATION_CREDENTIALS=\"C:\\Users\\Kevin Yu\\Downloads\\neural-water-328310-961eb49399c0.json\";python detection\\google_cloud.py";
+//		Process p = Runtime.getRuntime().exec(command);
+//		System.out.println("out");
 		Preprocess processor = new Preprocess(args[0], args[1], args[2], args[3]);
+
+		processor.parseJsonFile();
 		MotionCompensation.ENTROPY_THRESHOLD = Double.parseDouble(args[4]);
 		AudioFrame.AUDIO_LEVEL_THRESHOLD_UPPER = Integer.parseInt(args[5]);
 		AudioFrame.AUDIO_LEVEL_THRESHOLD_LOWER = Integer.parseInt(args[6]);
@@ -332,9 +473,6 @@ public class Preprocess {
 		List<Shot> shots = processor.getShots(shotBoundaries);
 		List<Scene> scenes = processor.getScenesFromShots(shots);
 		System.out.println(scenes.toString());
-
-
-		//ad detection
 
 		//write new files
 		processor.writeRgbFile(scenes);
